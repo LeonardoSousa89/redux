f0dc147a43899fb22e1f9ad57cb87c89bec801a7
Mendes Sousa <leoprofessionallogytech@outlook.com>
2023-03-02 01:56:06 -0300

    @v1.0.7

--- redux_aprofundamento_solucao_sem_redux/src/components/footer/footer.jsx
+++ redux_aprofundamento_solucao_sem_redux/src/components/footer/footer.jsx
@@ -7,7 +7,7 @@ export default class Footer extends Component{
     render(){
         return(
             <footer className="app-footer">
-                footer component
+                <strong>&copy; Techno Compra Total - 2023 possui todos os direitos, Salvador - BA</strong>
             </footer>
         )
     }
